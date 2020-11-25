c34a7d8a6a2b161d229dd72c0e49d5f11fa3e3a3
berhane beyene <monoxica2004@yahoo.com>
2020-11-24 22:25:56 -0600

    modified

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -12,12 +12,10 @@ function Navbar() {
                         aria-controls="nav-home" aria-selected="true">
                         Home
                     </Link>
-
                     <Link className="nav-item nav-link" id="nav-profile-tab" dataToggle="tab" to="/portfolio" role="tab"
                         aria-controls="nav-profile" aria-selected="false">
                         Portofolio
                     </Link>
-
                     <Link className="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" to="/contact" role="tab"
                         aria-controls="nav-contact" aria-selected="false">
                         Contact
